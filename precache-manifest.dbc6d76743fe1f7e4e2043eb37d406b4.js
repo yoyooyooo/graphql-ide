4e3813eb1c9059bcfc6a04ede963f94f9fa371bf
yoyooyooo <315915093@qq.com>
2020-10-09 03:11:04 +0000

    Deploying to gh-pages from  @ a483e9e2528e6b852471035a6dedd33e8ed749a6 🚀

---
+++ precache-manifest.dbc6d76743fe1f7e4e2043eb37d406b4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0175df957e0fd800956a",
+    "url": "/vendors.chunk.css"
+  },
+  {
+    "revision": "0175df957e0fd800956a",
+    "url": "/vendors.async.js"
+  },
+  {
+    "revision": "7ec7a0ef5d0c6738ea02",
+    "url": "/layouts__index.async.js"
+  },
+  {
+    "revision": "680e65f48ffb8086fe06",
+    "url": "/p__index.async.js"
+  },
+  {
+    "revision": "1410ebcd6edc57ab30fb",
+    "url": "/umi.css"
+  },
+  {
+    "revision": "1410ebcd6edc57ab30fb",
+    "url": "/umi.js"
+  },
+  {
+    "revision": "e17bdb114d791c77db3b76094750fa44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bf05b4e6659c56ed31a728cb72b13a5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "9ef055bcf0c4b1f0ac5663173730b010",
+    "url": "/pwacompat.min.js"
+  }
+];
